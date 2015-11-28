161b27a72d0382d49e4e683c0ddbc05ed560bf4c
Yuki Takeda <yuki@imaginaryshort.com>
2015-11-28 23:44:51 +0900

    update sql

--- server/db.sql
+++ server/db.sql
@@ -3,6 +3,6 @@ CREATE TABLE User(
   `Id`        INTEGER DEFAULT 0 PRIMARY KEY,
   `Name`      TEXT DEFAULT '',
   `Hid`       INTEGER DEFAULT 0,
-  `Status`    ENUM('', 'still', 'walk', 'run', 'bicycle', 'sleep', 'meal') DEFAULT '',
+  `Status`    ENUM('', 'still', 'walk', 'run', 'bicycle', 'sleep', 'meal', 'refrigerator') DEFAULT '',
   `Updated`   TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
