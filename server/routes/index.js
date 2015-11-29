cae09f0ac12ca58647d6f33add3b5625a3771e72
Yuki Takeda <yuki@imaginaryshort.com>
2015-11-29 09:58:15 +0900

    fix

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,14 +12,14 @@ var connection = mysql.createConnection({
 
 /* GET : Get all user status */
 router.get('/', function(req, res, next) {
-  connection.query('SELECT Status.UserId, User.HomeId, User.Name, Status.Status, Status.Updated FROM Status INNER JOIN User ON Status.UserId = User.Id', function (error, results, fields) {
+  connection.query('SELECT Status.UserId, User.HomeId, User.Name, User.Order, Status.Status, Status.Updated FROM Status INNER JOIN User ON Status.UserId = User.Id', function (error, results, fields) {
     res.send(JSON.stringify({users: results}));
   });
 });
 
 /* GET : Get all user status belonging to the hid */
 router.get('/hid', function(req, res, next) {
-  connection.query('SELECT Status.UserId, User.HomeId, User.Name, Status.Status, Status.Updated FROM Status INNER JOIN User ON Status.UserId = User.Id WHERE HomeId = ' + req.query.hid, function (error, results, fields) {
+  connection.query('SELECT Status.UserId, User.HomeId, User.Name, User.Order, Status.Status, Status.Updated FROM Status INNER JOIN User ON Status.UserId = User.Id WHERE HomeId = ' + req.query.hid, function (error, results, fields) {
     res.send(JSON.stringify({users: results}));
   });
 });
