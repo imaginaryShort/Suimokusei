9c439cf7cc6aa4089516a784953bb1667c667544
Yuki Takeda <yuki@imaginaryshort.com>
2015-12-05 12:53:21 +0900

    fix status

--- server/routes/status.js
+++ server/routes/status.js
@@ -23,7 +23,7 @@ router.get('/add', function(req, res, next) {
   connection.query('INSERT INTO `Status` (SensorId, Status) VALUES (' + req.query.seid + ',\'' + req.query.status + '\') ON DUPLICATE KEY UPDATE Status = \'' + req.query.status + '\', Updated = CURRENT_TIMESTAMP', function (error, results, fields) {
     //Send notification
     connection.query('SELECT Sensor.SuimokuseiId, Sensor.UserId, User.Order FROM Sensor INNER JOIN User ON Sensor.UserId = User.Id WHERE Sensor.Id = ' + req.query.seid, function (error, results, fields) {
-      results["Status"] = req.query.status;
+      results[0]["Status"] = req.query.status;
       res.send(JSON.stringify({results: results}));
     });
   });
