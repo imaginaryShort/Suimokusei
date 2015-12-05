19a88dfafd8c169325b02cca35e2a3193533d01a
Yuki Takeda <yuki@imaginaryshort.com>
2015-12-05 14:22:57 +0900

    fix raspi

--- raspi/suimokusei.py
+++ raspi/suimokusei.py
@@ -2,7 +2,9 @@ import json
 import websocket
 
 def on_message(ws, message):
-    print message
+    data = json.load(message)
+    order = data[0]["Order"]
+    suzuran.set_color_by_order(order)
 
 def on_error(ws, error):
     print error
@@ -15,7 +17,33 @@ def on_open(ws):
     #ws.close()
 
 
+class NeoPixelConfig(object):
+    # LED branches config:
+    LED_COUNT = 5
+    LED_PIN = 18
+    LED_FREQ_HZ = 800000
+    LED_DMA = 5
+    LED_BRIGHTNESS = 255
+    LED_INVERT = False
 
+    def __init__(self):
+        self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+        self.strip.begin()
+
+    def set_color_by_order(self, order):
+        c = Color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.strip.setPixelColor(order-1, c)
+        self.strip.show()
+
+    def random_all(self):
+        for x in range(LED_COUNT):
+            self.strip.setPixelColor(x, Color(  random.randint(0, 255),
+                                                random.randint(0, 255),
+                                                random.randint(0, 255)))
+            self.strip.show()
+
+
+suzuran = NeoPixelConfig();
 if __name__ == "__main__":
     f = open("env.json")
     env = json.load(f)
@@ -23,8 +51,8 @@ if __name__ == "__main__":
 
     #websocket.enableTrace(True)
     ws = websocket.WebSocketApp(env["server"],
-                                on_message = on_message,
-                                on_error = on_error,
-                                on_close = on_close)
+            on_message = on_message,
+            on_error = on_error,
+            on_close = on_close)
     ws.on_open = on_open
     ws.run_forever()
